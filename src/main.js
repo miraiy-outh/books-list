7a4ce9c203fed017ccb86b2a4783cca6321ba75b
miraiy-outh <shokolatemeur@mail.ru>
2025-09-30 17:31:27 +0300

    desctop view

--- src/main.js
+++ src/main.js
@@ -1,24 +1,57 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { books } from "./data.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const container = document.querySelector("#books");
 
-setupCounter(document.querySelector('#counter'))
+function createBookCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  if (book.groupId === 1) {
+    card.classList.add("main");
+  }
+  if (book.groupId === 2) {
+    card.classList.add("secondary");
+  }
+  card.id = `book${book?.id}-group${book?.groupId || 0}`;
+
+  const coverContainer = document.createElement("div");
+  coverContainer.classList.add("card__cover");
+  coverContainer.style.backgroundImage = "url('/public/background.png')";
+  const coverImage = document.createElement("img");
+  coverImage.classList.add("card__cover__image");
+  coverImage.src = book.image;
+
+  coverContainer.appendChild(coverImage);
+
+  const infoContainer = document.createElement("div");
+  infoContainer.classList.add("card__info");
+  const title = document.createElement("h2");
+  title.classList.add("card__info__title");
+  title.textContent = book.title;
+  const author = document.createElement("h3");
+  author.classList.add("card__info__author");
+  author.textContent = book.author;
+  const description = document.createElement("p");
+  description.classList.add("card__info__description");
+  description.textContent = book.description;
+
+  infoContainer.appendChild(title);
+  infoContainer.appendChild(author);
+  infoContainer.appendChild(description);
+
+  card.appendChild(coverContainer);
+  card.appendChild(infoContainer);
+  container.appendChild(card);
+}
+
+function renderCards(books) {
+  books.forEach((book) => {
+    createBookCard(book);
+  });
+}
+
+function initLoadMore() {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderCards(books);
+  initLoadMore();
+});
